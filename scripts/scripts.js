2fd009882ab7b325b4710c48d1b013b0ec06a1fd
Giuseppe Borgonovo <gborgono@adobe.com>
2023-12-14 11:10:14 +0100

    add experiment plugin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,6 +16,23 @@ import {
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
+ * added borgo x generative experiment AI 
+ */
+const AUDIENCES = {
+  mobile: () => window.innerWidth < 600,
+  desktop: () => window.innerWidth >= 600,
+  // define your custom audiences here as needed
+};
+
+window.hlx.plugins.add('experimentation', {
+  condition: () => getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length,
+  options: { audiences: AUDIENCES },
+  url: '/plugins/experimentation/src/index.js',
+});
+
+/**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
