6ce5afe27c8ec337e0bf9ebe43cdbf5d3f3c23bd
Giuseppe Borgonovo <gborgono@adobe.com>
2024-01-26 15:05:20 +0100

    video update

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,20 +1,29 @@
-import { buildFigure } from '../../scripts/scripts.js';
+import decodeHTML from '../../scripts/decode-html.js';
 
-export default function decorate(block) {
-  if (block.classList.contains('is-loaded')) {
-    return;
+export default async function decorate(block) {
+  // Find the the embed code and caption if they exist
+  const videoEmbed = block.querySelector('div > div:first-child') !== null ? block.querySelector('div > div:first-child').innerText : null;
+  const videoCaption = block.querySelector('div > div:nth-child(2)') !== null ? block.querySelector('div > div:nth-child(2)').innerText : null;
+
+  // Decode the encoded embed code
+  const makeEmbed = decodeHTML(videoEmbed);
+
+  // Create the figure and populate it with the embed wrapper
+  const makeFigure = document.createElement('figure');
+  makeFigure.className = 'cmp-media-figure';
+  makeFigure.innerHTML = `<div class="cmp-media-figure__embed">${makeEmbed}</div>`;
+  makeFigure.querySelector('iframe').className = 'cmp-media-figure__iframe';
+  block.parentNode.insertBefore(makeFigure, block);
+
+  // If there is a caption element, then create a figcaption
+  // element and add it in the figure beneath the embed wrapper
+  if (videoCaption != null) {
+    const makeFigcaption = document.createElement('figcaption');
+    makeFigcaption.className = 'cmp-media-figure__caption';
+    makeFigcaption.innerHTML = videoCaption;
+    makeFigure.appendChild(makeFigcaption);
   }
-  const poster = block.querySelector('img') ? `poster="${block.querySelector('img').src}"` : '';
-  const a = block.querySelector('a');
-  const videoSrc = a.href;
-  const video = document.createElement('div');
-  const figure = buildFigure(block.firstChild.firstChild);
-  video.classList.add('video-wrapper');
-  video.innerHTML = `<video controls preload="none" ${poster}>
-    <source src="${videoSrc}" type="video/mp4">
-  </video>`;
-  block.innerHTML = '<figure class="figure"></figure>';
-  block.firstChild.prepend(video);
-  block.firstChild.append(figure.querySelector('figcaption'));
-  block.classList.add('is-loaded');
+
+  // And we don’t need that original block anymore, so we remove it
+  block.remove();
 }
